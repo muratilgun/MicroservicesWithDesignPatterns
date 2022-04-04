e2d5dab9e2b441bf1098385f6df8dcec0b85b183
muratilgun <muratilgun34@gmail.com>
2022-04-04 22:57:25 +0300

    Created Order & OrderItem & Address Models

---
+++ MicroservicesWithDesignPatterns/Order.API/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order.API.Models
+{
+    
+    public class Address
+    {
+        public string Line { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+    }
+}
