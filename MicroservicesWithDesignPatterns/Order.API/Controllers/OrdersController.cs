76d42907a67916bb30b600553d76b89576616831
muratilgun <muratilgun34@gmail.com>
2022-04-06 22:50:56 +0300

    Created Dtos

---
+++ MicroservicesWithDesignPatterns/Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok();
+        }
+    }
+}
