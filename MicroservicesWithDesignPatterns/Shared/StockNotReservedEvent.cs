2a477538340b7d32b24b61e571ca0d912378ba8b
muratilgun <muratilgun34@gmail.com>
2022-04-07 19:44:33 +0300

    OrderCreatedEventConsumer Consume method

--- MicroservicesWithDesignPatterns/Shared/StockNotReservedEvent.cs
+++ MicroservicesWithDesignPatterns/Shared/StockNotReservedEvent.cs
@@ -3,7 +3,6 @@
     public class StockNotReservedEvent
     {
         public int OrderId { get; set; }
-        public string BuyerId { get; set; }
         public string Message { get; set; }
     }
 }
