2684f76f4e6ef81cf88662e028e63559d65fbf54
muratilgun <muratilgun34@gmail.com>
2022-04-07 17:35:09 +0300

    OrderCreatedEventConsumer

---
+++ MicroservicesWithDesignPatterns/Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Shared;
+
+namespace Stock.API.Consumers
+{
+    public class OrderCreatedEventConsumer :  IConsumer<OrderCreatedEvent>
+    {
+        public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MicroservicesWithDesignPatterns/Stock.API/Startup.cs
+++ MicroservicesWithDesignPatterns/Stock.API/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Stock.API.Models;
 
@@ -28,6 +29,14 @@ namespace Stock.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMassTransit(x =>
+            {
+                x.UsingRabbitMq((contex, cfg) =>
+                {
+                    cfg.Host(Configuration.GetConnectionString("RabbitMQ"));
+                });
+            });
+            services.AddMassTransitHostedService();
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseInMemoryDatabase("StockDb");
