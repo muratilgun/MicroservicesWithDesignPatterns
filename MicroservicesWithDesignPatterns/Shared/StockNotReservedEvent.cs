70878c5dd1da0487afc0bb339d9135b46ff1b6fd
muratilgun <muratilgun34@gmail.com>
2022-04-07 19:22:30 +0300

    Event properties

--- MicroservicesWithDesignPatterns/Shared/StockNotReservedEvent.cs
+++ MicroservicesWithDesignPatterns/Shared/StockNotReservedEvent.cs
@@ -2,6 +2,8 @@
 {
     public class StockNotReservedEvent
     {
-        
+        public int OrderId { get; set; }
+        public string BuyerId { get; set; }
+        public string Message { get; set; }
     }
 }
--- MicroservicesWithDesignPatterns/Shared/StockReservedEvent.cs
+++ MicroservicesWithDesignPatterns/Shared/StockReservedEvent.cs
@@ -8,6 +8,9 @@ namespace Shared
 {
     public class StockReservedEvent
     {
-
+        public int OrderId { get; set; }
+        public string BuyerId { get; set; }
+        public PaymentMessage Payment { get; set; }
+        public List<OrderItemMessage> OrderItems { get; set; } = new List<OrderItemMessage>();
     }
 }
