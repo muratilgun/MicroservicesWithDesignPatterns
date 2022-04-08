26cb78d2e74d69571938aee375f2a7a620a05aca
muratilgun <muratilgun34@gmail.com>
2022-04-08 19:51:19 +0300

    Created StockController

--- MicroservicesWithDesignPatterns/Shared/RabbitMQSettingsConst.cs
+++ MicroservicesWithDesignPatterns/Shared/RabbitMQSettingsConst.cs
@@ -8,6 +8,7 @@ namespace Shared
 {
     public class RabbitMQSettingsConst
     {
+        public const string StockReservedEventQueueName = "stock-reserved-queue";
         public const string StockOrderCreatedEventQueueName = "stock-order-created-queue";
     }
 }
