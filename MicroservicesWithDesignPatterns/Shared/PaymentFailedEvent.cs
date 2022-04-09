ccdbe201578ab9c954d8cfc02fe0f5845f09f2e7
muratilgun <muratilgun34@gmail.com>
2022-04-09 18:48:27 +0300

    Created PaymentSuccessedEvent & PaymentFailedEvent

---
+++ MicroservicesWithDesignPatterns/Shared/PaymentFailedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared
+{
+    public class PaymentFailedEvent
+    {
+        public int OrderId { get; set; }
+        public string BuyerId { get; set; }
+        public string Message { get; set; }
+    }
+}
