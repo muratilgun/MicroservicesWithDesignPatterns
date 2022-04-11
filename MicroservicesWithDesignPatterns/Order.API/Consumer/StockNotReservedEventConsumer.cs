f5cd1dca3a1b85255e45a550ab3105ce9dbcd3f1
muratilgun <muratilgun34@gmail.com>
2022-04-11 21:23:34 +0300

    StockNotReservedEventConsumer

---
+++ MicroservicesWithDesignPatterns/Order.API/Consumer/StockNotReservedEventConsumer.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Order.API.Models;
+using Shared;
+
+namespace Order.API.Consumer
+{
+    public class StockNotReservedEventConsumer : IConsumer<StockNotReservedEvent>
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger<StockNotReservedEventConsumer> _logger;
+
+        public StockNotReservedEventConsumer(ILogger<StockNotReservedEventConsumer> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public async Task Consume(ConsumeContext<StockNotReservedEvent> context)
+        {
+            Models.Order order = await _context.Orders.FindAsync(context.Message.OrderId);
+            if (order != null)
+            {
+                order.Status = OrderStatus.Fail;
+                order.FailMessage = context.Message.Message;
+                await _context.SaveChangesAsync();
+                _logger.LogInformation($"Order (Id = {context.Message.OrderId}) status changed : {order.Status}");
+            }
+            else
+            {
+                _logger.LogError($"Order (Id = {context.Message.OrderId}) not found");
+            }
+        }
+    }
+}
--- MicroservicesWithDesignPatterns/Shared/RabbitMQSettingsConst.cs
+++ MicroservicesWithDesignPatterns/Shared/RabbitMQSettingsConst.cs
@@ -9,6 +9,7 @@ namespace Shared
     public class RabbitMQSettingsConst
     {
         public const string StockReservedEventQueueName = "stock-reserved-queue";
+        public const string OrderStockNotReservedEventQueueName = "order-stock-not-reserved-queue";
         public const string StockOrderCreatedEventQueueName = "stock-order-created-queue";
         public const string OrderPaymentCompletedEventQueueName = "order-payment-completed-queue";
         public const string OrderPaymentFailedEventQueueName = "order-payment-failed-queue";
