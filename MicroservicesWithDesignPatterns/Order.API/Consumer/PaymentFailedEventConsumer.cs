64c99c94c60ae79f3c92475070cfde44ac7f93cc
muratilgun <muratilgun34@gmail.com>
2022-04-11 20:30:39 +0300

    PaymentFailedEventConsumer

---
+++ MicroservicesWithDesignPatterns/Order.API/Consumer/PaymentFailedEventConsumer.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Order.API.Models;
+using Shared;
+
+namespace Order.API.Consumer
+{
+    public class PaymentFailedEventConsumer : IConsumer<PaymentFailedEvent>
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger<PaymentCompletedEventConsumer> _logger;
+
+        public PaymentFailedEventConsumer(ILogger<PaymentCompletedEventConsumer> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task Consume(ConsumeContext<PaymentFailedEvent> context)
+        {
+            Models.Order order = await _context.Orders.FindAsync(context.Message.OrderId);
+            if (order != null)
+            {
+                order.Status = OrderStatus.Fail;
+                order.FailMessage = context.Message.Message;
+                await _context.SaveChangesAsync();
+                _logger.LogInformation($"Order (Id = {context.Message.OrderId}) status changed : {order.Status}");
+            }
+            else
+            {
+                _logger.LogError($"Order (Id = {context.Message.OrderId}) not found");
+            }
+        }
+    }
+}
--- MicroservicesWithDesignPatterns/Shared/RabbitMQSettingsConst.cs
+++ MicroservicesWithDesignPatterns/Shared/RabbitMQSettingsConst.cs
@@ -11,5 +11,6 @@ namespace Shared
         public const string StockReservedEventQueueName = "stock-reserved-queue";
         public const string StockOrderCreatedEventQueueName = "stock-order-created-queue";
         public const string OrderPaymentCompletedEventQueueName = "order-payment-completed-queue";
+        public const string OrderPaymentFailedEventQueueName = "order-payment-failed-queue";
     }
 }
