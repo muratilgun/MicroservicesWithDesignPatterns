4a76911f5be501a11e114ccf34d2f3707cdc8072
muratilgun <muratilgun34@gmail.com>
2022-04-14 22:15:12 +0300

    Created OrderStateInstance

---
+++ MicroservicesWithDesignPatterns/SagaStateMachineWorkerService/Model/OrderStateInstance.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Automatonymous;
+
+namespace SagaStateMachineWorkerService.Model
+{
+    public class OrderStateInstance : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+    }
+}
