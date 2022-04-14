4cee57d2ce782e186557380854cb06d9ca6364b2
muratilgun <muratilgun34@gmail.com>
2022-04-14 22:29:04 +0300

    OrderStateInstance implemented

--- MicroservicesWithDesignPatterns/SagaStateMachineWorkerService/Model/OrderStateInstance.cs
+++ MicroservicesWithDesignPatterns/SagaStateMachineWorkerService/Model/OrderStateInstance.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using Automatonymous;
 
 namespace SagaStateMachineWorkerService.Model
@@ -6,5 +8,26 @@ namespace SagaStateMachineWorkerService.Model
     public class OrderStateInstance : SagaStateMachineInstance
     {
         public Guid CorrelationId { get; set; }
+        public string CurrentState { get; set; }
+        public string BuyerId { get; set; }
+        public int OrderId { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreateDate { get; set; }
+        public override string ToString()
+        {
+            var properties = GetType().GetProperties();
+            var sb = new StringBuilder();
+            properties.ToList().ForEach(p =>
+            {
+                var value = p.GetValue(this, null);
+                sb.Append($"{p.Name} : {value}");
+            });
+            sb.Append("-----------------");
+            return sb.ToString();
+        }
     }
 }
