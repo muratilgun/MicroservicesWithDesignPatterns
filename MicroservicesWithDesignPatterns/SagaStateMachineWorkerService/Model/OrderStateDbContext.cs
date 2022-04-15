5eecd5baf3d700e6b211251b73459fb96ddaf6e1
muratilgun <muratilgun34@gmail.com>
2022-04-15 20:40:05 +0300

    OrderStateDbContext & OrderStateMap

---
+++ MicroservicesWithDesignPatterns/SagaStateMachineWorkerService/Model/OrderStateDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using MassTransit.EntityFrameworkCoreIntegration;
+using MassTransit.EntityFrameworkCoreIntegration.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace SagaStateMachineWorkerService.Model
+{
+    public class OrderStateDbContext : SagaDbContext
+    {
+        public OrderStateDbContext(DbContextOptions<OrderStateDbContext> options) : base(options)
+        {
+        }
+
+        protected override IEnumerable<ISagaClassMap> Configurations
+        {
+            get
+            {
+                yield return new OrderStateMap();
+
+            }
+        }
+    }
+}
---
+++ MicroservicesWithDesignPatterns/SagaStateMachineWorkerService/Model/OrderStateMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MassTransit.EntityFrameworkCoreIntegration.Mappings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SagaStateMachineWorkerService.Model
+{
+    public class OrderStateMap : SagaClassMap<OrderStateInstance>
+    {
+        protected override void Configure(EntityTypeBuilder<OrderStateInstance> entity, ModelBuilder model)
+        {
+            entity.Property(x => x.BuyerId).HasMaxLength(256);
+        }
+    }
+}
