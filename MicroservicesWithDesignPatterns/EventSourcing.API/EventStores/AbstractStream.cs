c55a819c89ab609daf54517c83fb59815ae01f03
muratilgun <muratilgun34@gmail.com>
2022-04-29 00:53:30 +0300

    AbstractStream

---
+++ MicroservicesWithDesignPatterns/EventSourcing.API/EventStores/AbstractStream.cs
@@ -0,0 +1,39 @@
+﻿using EventSourcing.Shared.Events;
+using EventStore.ClientAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EventSourcing.API.EventStores
+{
+    public abstract class AbstractStream
+    {
+        protected readonly LinkedList<IEvent> Events = new LinkedList<IEvent>();
+
+        private string _streamName { get; }
+        private readonly IEventStoreConnection _eventStoreConnection;
+
+        protected AbstractStream(IEventStoreConnection eventStoreConnection, string streamName)
+        {
+            _eventStoreConnection = eventStoreConnection;
+            _streamName = streamName;
+        }
+
+        public async Task SaveAsync()
+        {
+            var newEvents = Events.ToList().Select(x => new EventData(
+                Guid.NewGuid(),
+                x.GetType().Name,
+                true,
+                Encoding.UTF8.GetBytes(JsonSerializer.Serialize(x, inputType: x.GetType())),
+                Encoding.UTF8.GetBytes(x.GetType().FullName))).ToList();
+
+            await _eventStoreConnection.AppendToStreamAsync(_streamName, ExpectedVersion.Any, newEvents);
+
+            Events.Clear();
+        }
+    }
+}
