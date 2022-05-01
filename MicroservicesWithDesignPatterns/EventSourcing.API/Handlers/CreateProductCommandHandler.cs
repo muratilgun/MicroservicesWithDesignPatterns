dffb0d0d9f874c171c7dfcb45ff563fc2b8ba890
muratilgun <muratilgun34@gmail.com>
2022-05-01 10:38:54 +0300

    Create and Delete product Handlers

---
+++ MicroservicesWithDesignPatterns/EventSourcing.API/Handlers/CreateProductCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EventSourcing.API.Commands;
+using EventSourcing.API.EventStores;
+using MediatR;
+
+namespace EventSourcing.API.Handlers
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand>
+    {
+        private readonly ProductStream _productStream;
+
+        public CreateProductCommandHandler(ProductStream productStream)
+        {
+            _productStream = productStream;
+        }
+
+        public async Task<Unit> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            _productStream.Created(request.CreatedProductDto);
+
+            await _productStream.SaveAsync();
+
+            return  Unit.Value;
+        }
+    }
+}
