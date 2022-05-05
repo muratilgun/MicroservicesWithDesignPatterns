f850a05c693b722e64a8d1258342cdf5cc6d8649
muratilgun <muratilgun34@gmail.com>
2022-05-05 22:53:01 +0300

    GetRetryPolicy

--- MicroservicesWithDesignPatterns/ServiceA.API/Startup.cs
+++ MicroservicesWithDesignPatterns/ServiceA.API/Startup.cs
@@ -1,16 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace ServiceA.API
 {
@@ -36,6 +36,24 @@ namespace ServiceA.API
             {
                 opt.BaseAddress = new Uri("https://localhost:5003/api/products/");
             });
+        }
+
+        private IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions.HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound).WaitAndRetryAsync(5,
+                    retryAttempt =>
+                    {
+                        Debug.WriteLine($"Retry Count : {retryAttempt}");
+                        return TimeSpan.FromSeconds(10);
+                    },onRetryAsync:onRetryAsync);
+        }
+
+        private Task onRetryAsync(DelegateResult<HttpResponseMessage> arg1, TimeSpan arg2)
+        {
+            Debug.WriteLine($"Request is made again: {arg2.TotalMilliseconds}");
+            return Task.CompletedTask;
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
